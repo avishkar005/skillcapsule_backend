3875deefd3f735c64d771bd46e413856e22fb1e5
avishkar005 <avishkarchoundkar4@gmail.com>
2026-03-05 08:52:20 +0530

    Add OAuth2 client repository bean

---
+++ src/main/java/com/skillcapsule/skillcapsule/config/OAuth2ClientConfig.java
@@ -0,0 +1,33 @@
+package com.skillcapsule.skillcapsule.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+
+        ClientRegistration google = ClientRegistration.withRegistrationId("google")
+                .clientId("383076719405-iqf0eopfu9t89fur3q8fqumende28699.apps.googleusercontent.com")
+                .clientSecret("REPLACE_WITH_NEW_SECRET")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/google")
+                .scope("email", "profile")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://oauth2.googleapis.com/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName("sub")
+                .clientName("Google")
+                .build();
+
+        return new InMemoryClientRegistrationRepository(google);
+    }
+}
